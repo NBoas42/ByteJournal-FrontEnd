1d98a31f7f81f089ed0e8384d72859a7929414be
Nate Boas <nbboas@gmail.com>
2026-05-14 19:43:36 -0700

    Progress on Login Page

---
+++ src/components/account/LoginForm.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="login-form-wrapper">
+        <h1 class="login-form-title">Welcome back</h1>
+        <p> Sign in to your account to continue</p>
+
+        <hr/>
+
+        <h5>Email Address</h5>
+        <Input type="email" placeholder="Enter your email" />
+
+        <p>Password</p>
+        <Input type="password" placeholder="Enter your password" />
+
+        <Button class="login-button">Sign in</Button>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
+
+</script>
+
+<style scoped>
+@import "tailwindcss";
+
+.login-form-wrapper {
+    /*Positioning*/ @apply inline-flex flex-col items-center;
+    /*size */ @apply w-1/2;
+}
+
+.login-form-title {
+    /*text size*/ @apply text-2xl font-bold;
+}
+.login-button {
+    /*size*/ @apply w-full;
+}
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="login-wrapper">
+        <div class="pane-left"/>
+        <div class="pane-right">
+            <LoginForm />
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import LoginForm from '@/components/account/LoginForm.vue'
+
+</script>
+
+<style scoped>
+@import "tailwindcss";
+
+.login-wrapper {
+    /*Positioning*/ @apply flex items-center justify-start; /*items is vertical and justify is horizontal*/
+    /*size*/        @apply h-screen w-screen;
+}
+.pane-left {
+    /*size*/        @apply h-screen w-1/2;
+    /*color*/       @apply bg-linear-to-br from-indigo-700 to-purple-700;
+}
+
+.pane-right {
+    /*Positioning*/ @apply flex items-center justify-center; /*items is vertical and justify is horizontal*/
+    /*size*/        @apply h-screen w-1/2;
+    /*color*/       background-color: #f8f8f8;
+}
+</style>
+
