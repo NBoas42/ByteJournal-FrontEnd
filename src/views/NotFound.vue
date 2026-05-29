bf291012fe54d56bfceb2b208fbd6f1b3287cfb0
Nate Boas <nbboas@gmail.com>
2026-05-28 23:20:39 -0700

    Better 404 Page

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,5 +1,24 @@
 <template>
-    <div>
-        <h1>404 Page Not Found</h1>
+    <div class="not-found-wrapper">
+        <h1 class="not-found-title">404</h1>
+        <h1 class="not-found-subtitle">Page Not Found</h1>
+        <p>The Page you are looking for does not exist.</p>
     </div>
 </template>
+
+<style scoped>
+@reference "../App.css";
+
+.not-found-wrapper {
+    @apply flex flex-col items-center justify-center;
+    @apply h-screen w-screen;
+}
+
+.not-found-title {
+    @apply text-4xl font-bold;
+}
+
+.not-found-subtitle {
+    @apply text-xl;
+}
+</style>
